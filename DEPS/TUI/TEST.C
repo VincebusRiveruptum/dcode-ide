6e149ff1194e59f42f5ce17f88159ad3f274b479
VincebusRiveruptum <q3map_2@live.cl>
2026-01-22 00:08:18 -0300

    add: color rectangles text

--- DEPS/TUI/TEST.C
+++ DEPS/TUI/TEST.C
@@ -8,8 +8,15 @@ void test_tui(){
 void tg_t_initTests(){
     printf("This is just a blank test\n");
 
-    //tg_drawRectangle(0, 0, TUI_COLS - 1, TUI_ROWS - 1, T_COLOR_BLUE, T_COLOR_LIGHT_BLUE, '°', false);
     tg_fill(T_COLOR_BLUE, T_COLOR_LIGHT_BLUE, '°');
-    //tg_drawRectangle(3, 3, 20, 10, T_COLOR_CYAN, T_COLOR_BLACK, 219, T_COLOR_CYAN, false, TUI_DRAW_BORDER_SIMPLE);
+    
+    tg_colorRectangles();
+    
+}
+
+void tg_colorRectangles(){
     tg_drawRectangle(3, 3, 20, 10, T_COLOR_CYAN, T_COLOR_BLACK, 219, T_COLOR_CYAN, T_COLOR_BLACK, true, TUI_DRAW_BORDER_SIMPLE);
+    tg_drawRectangle(30, 7, 50, 20, T_COLOR_MAGENTA, T_COLOR_BLACK, 219, T_COLOR_MAGENTA, T_COLOR_BLACK, true, TUI_DRAW_BORDER_SIMPLE);
+    tg_drawRectangle(23, 23, 40, 40, T_COLOR_RED, T_COLOR_BLACK, 219, T_COLOR_RED, T_COLOR_BLACK, true, TUI_DRAW_BORDER_SIMPLE);
+    tg_drawRectangle(60, 20, 70, 30, T_COLOR_LIGHT_YELLOW, T_COLOR_BLACK, 219, T_COLOR_BROWN, T_COLOR_BLACK, true, TUI_DRAW_BORDER_SIMPLE);
 }

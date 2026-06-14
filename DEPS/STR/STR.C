6c6b8cb103b21b30847fea3ddd6205fae374c795
Vincebus Riveruptum <q3map_2@live.cl>
2026-06-14 19:43:20 -0400

    chg: str func

--- DEPS/STR/STR.C
+++ DEPS/STR/STR.C
@@ -1,30 +1,50 @@
 #include "STR.H"
 
+// Iterative version
+void strstrip(char *str) {
+	char *src = NULL;
+	char *dst = NULL;
+
+    if (!str) return;
+
+    src = str;
+    dst = str;
+
+    while (*src != '\0') {
+        if (!isspace((unsigned char)*src)) {
+            *dst++ = *src;
+        }
+        src++;
+    }
+    *dst = '\0';
+}
 
 // Removes spaces in between 
-void *strstrip(char *str){
-		
-	
+// This approach needs to be refactored.. long string could
+// overflow the stack.
+
+void strstrip_old(char *str){
+	char *auxptr;
+
 	if(!str || *str == '\0')
 		return;
 		
-
 	// One step to strip
-
-	if(isspace(*str){
-		*str = *str + 1
-		str++;
+	if(isspace(*str)){
 		// We move to the left the rest of chars.
-		while(str && (*str) != '\0' && !isspace(*str)){
-			*str = *(str + 1);
-			str++;  
+		auxptr = str;
+
+		while(*auxptr != '\0'){
+			*auxptr = *(auxptr + 1);
+			auxptr++;  
 		}
-		
-		if(str) strstrip(str);
+
+		strstrip(str);
+	}else{
+		strstrip(str+1);
 	}
 	
 	return;
-
 }
 
 // Slice between a range in a string
@@ -34,7 +54,7 @@ int strnslice(char *str, size_t from, size_t to, size_t strlen ){
 	if(to > strlen) to = strlen - 1;
 	if(from > to) from = to - 1;
 	
-	gap = to - from;<
+	gap = to - from;
 	
 	for(i=from;i<to;i++){
 		str[i] = str[i+gap];
@@ -49,10 +69,18 @@ int strnslice(char *str, size_t from, size_t to, size_t strlen ){
 // Join
 
 // Splice
+#ifdef STANDALONE
+
 int main(){
-	
+	char str[64] = "This is another string";
+
+	printf("\n%s", str);
+
+	strstrip(str);
+
+	printf("\n%s", str);
+
 	return 0;
 }
 
-
-
+#endif

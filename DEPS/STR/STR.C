6d45357d35bbbc171894c6fda2a21fe7bef278f0
VincebusRiveruptum <q3map_2@live.cl>
2026-06-15 22:45:06 -0400

    add: more string helpers

--- DEPS/STR/STR.C
+++ DEPS/STR/STR.C
@@ -1,12 +1,36 @@
+/*
+	=== Vincebus's STRING helpers ===
+		version 0.5 - forever WIP !
+	=================================
+
+	Use them at your own risk !
+	Jun 2026.
+
+	strsplit
+	strinv
+	strjoinint
+	strjoinfloat
+	strjoinfloat
+	strjoindouble
+	strjoinstr
+	strjoin
+	strltrim
+	strrtrim
+	strtrim
+	strstrip
+	strnslice
+*/
+
 #include "STR.H"
 
 // 
 #define TYPE_INT 0
 #define TYPE_CHAR 1
 #define TYPE_FLOAT 2
-#define TYPE_STR 3
+#define TYPE_DOUBLE 3
+#define TYPE_STR 4
 
-// Splits string into array of string by a separator
+// Splits string into array of string by a separator AND null terminated
 char *strsplit(char *str, const char *separator){
 	return NULL;
 }
@@ -59,7 +83,6 @@ char *strjoinint(int *arr, size_t arrlen, char *separator){
 		len += numlen;
 	}
 
-	
 	newstr = (char*)malloc(sizeof(char)*len);
 
 	if(!newstr) return NULL;
@@ -80,12 +103,13 @@ char *strjoinint(int *arr, size_t arrlen, char *separator){
 
 	return newstr;
 }
+
 // Joins array into a string by a separator
-char *strjoindouble(double *arr, size_t arrlen, char *separator){
+char *strjoinfloat(float *arr, size_t arrlen, char *separator){
 	char *newstr = NULL;
 	size_t len = 0;
 	int i;
-	char numbuff[48];
+	char numbuff[64];
 	int numlen;
 
 	if(!separator) return NULL;
@@ -93,26 +117,29 @@ char *strjoindouble(double *arr, size_t arrlen, char *separator){
 	// We count the number of chars so we can get 
 	// the total length for allocating our new string
 	for(i=0; i<arrlen; i++){
-		memset(numbuff, '\0', 48);
-		sprintf(numbuff, "%f%s", arr[i], separator);
+		memset(numbuff, '\0', 64);
+		if(i<arrlen-1){
+			sprintf(numbuff, "%.5f%s", arr[i], separator);
+		}else{
+			sprintf(numbuff, "%.5f", arr[i]);
+		}
 		numlen = strlen(numbuff);
 		len += numlen;
 	}
 
-	
 	newstr = (char*)malloc(sizeof(char)*len);
 
 	if(!newstr) return NULL;
 
 	memset(newstr, '\0', len);
-	memset(numbuff, '\0', 48);
+	memset(numbuff, '\0', 64);
 
 	// We start appeding
 	for(i=0; i<arrlen;i++){
 		if(i<arrlen-1){
-			sprintf(numbuff, "%f%s", arr[i], separator);
+			sprintf(numbuff, "%.5f%s", arr[i], separator);
 		}else{
-			sprintf(numbuff, "%d", arr[i]);
+			sprintf(numbuff, "%.5f", arr[i]);
 		}
 
 		strcat(newstr, numbuff);
@@ -121,19 +148,102 @@ char *strjoindouble(double *arr, size_t arrlen, char *separator){
 	return newstr;
 }
 
-char *strjoin(void *arr, unsigned char type, char *separator){
+char *strjoindouble(double *arr, size_t arrlen, char *separator){
+	char *newstr = NULL;
+	size_t len = 0;
+	int i;
+	char numbuff[64];
+	int numlen;
+
+	if(!separator) return NULL;
+
+	// We count the number of chars so we can get 
+	// the total length for allocating our new string
+	for(i=0; i<arrlen; i++){
+		memset(numbuff, '\0', 64);
+		if(i<arrlen-1){
+			sprintf(numbuff, "%.5f%s", arr[i], separator);
+		}else{
+			sprintf(numbuff, "%.5f", arr[i]);
+		}
+		numlen = strlen(numbuff);
+		len += numlen;
+	}
+
+	newstr = (char*)malloc(sizeof(char)*len);
+
+	if(!newstr) return NULL;
+
+	memset(newstr, '\0', len);
+	memset(numbuff, '\0', 64);
+
+	// We start appeding
+	for(i=0; i<arrlen;i++){
+		if(i<arrlen-1){
+			sprintf(numbuff, "%.5f%s", arr[i], separator);
+		}else{
+			sprintf(numbuff, "%.5f", arr[i]);
+		}
+
+		strcat(newstr, numbuff);
+	}
+
+	return newstr;
+}
+
+// Joins array of pointed strings into a string by a separator
+char *strjoinstr(char **arr, size_t arrlen, char *separator){
+	char *newstr = NULL;
+	size_t len = 0;
+	int i;
+	size_t bufflen;
+
+	if(!separator) return NULL;
+
+	// We count the number of chars so we can get 
+	// the total length for allocating our new string
+	for(i=0; i<arrlen; i++){
+		if(!arr[i]) continue;
+
+		bufflen += (strlen(arr + i) + strlen(separator));
+	}
+
+	newstr = (char*)malloc(sizeof(char)*bufflen);
+
+	if(!newstr) return NULL;
+
+	memset(newstr, '\0', bufflen);
+
+	bufflen = 0;
+	// We start appeding
+	for(i=0; i<arrlen;i++){
+		if(!arr[i]) continue;
+
+		if(i<arrlen-1){
+			bufflen += sprintf(newstr + bufflen, "%s%s", arr[i], separator);
+		}else{
+			bufflen += sprintf(newstr + bufflen, "%s", arr[i]);
+		}
+	}
+
+	return newstr;
+}
+
+char *strjoin(void *arr,unsigned char type, size_t arrlen,  char *separator){
 	if(!arr) return NULL;
 	if(!separator) return NULL;
 
 	switch(type){
 		case TYPE_INT:
-			return strjoinint((int*)arr, 10, separator);
+			return strjoinint((int*)arr, arrlen, separator);
 		case TYPE_CHAR:
-			return strjoinint((char*)arr, 20, separator);
+			return (char*)arr;	// lol
 		case TYPE_FLOAT:
-			return strjoinint((float*)arr, 20, separator);
+			return strjoinfloat((float*)arr, arrlen, separator);
+		case TYPE_DOUBLE:
+			return strjoindouble((double*)arr, arrlen, separator);
 		case TYPE_STR:
-			return strjoinint((char*)arr, 20, separator);
+			return strjoinstr((char*)arr, arrlen, separator);
 		default:
 			return NULL;
 	}
@@ -206,15 +316,43 @@ char *strnslice(char *str, size_t from, size_t step, size_t strlen ){
 int main(){
 	char str[64] = "This is another string";
 	char *renastr = "WHo's Renamon?";
-	int testnums[9] = {10, 25,40, 8, 1, 3 ,666, -5, -5999};
+	int testnums[11] = {10, 25,40, 8, 1, 3, 0 ,666, -5, -5999};
+	float testfloats[4] = { 1.8, 4.32, 7.14, 83.33 };
+	double testdoubles[4] = { 0.25, 3.14693, 16.666, 89.362 };
 
+	char *teststrs[5];
+
+	char *strLong = "\tLong One";
+	char *strLong2 = "Another one, but well\t\t";
+	char *strLong3 = "          Both sides   ";
+	
+	teststrs[0] = strdup("This is a string");
+	teststrs[1] = strdup("This is another");
+	teststrs[2] = strdup("An another");
+	teststrs[3] = strdup("Farts");
+	teststrs[4] = strdup("Foobar");
+
+	// SAMPLE STR
 	printf("\n%s", str);
 	
+	// INVERT
 	printf("\n%s", 	strinv(str));
-	
+
+	// SLICE 
 	printf("\n%s", strnslice(renastr, 6,7, strlen(renastr)));
 	
-	printf("\n%s", strjoinint(testnums, 9, ", "));
+	// JOIN
+	printf("\n[%s]", strjoinint(testnums, 9, ", "));
+	printf("\n[%s]", strjoin(teststrs, TYPE_STR, 5, ", "));
+	printf("\n[%s]", strjoin(testfloats, TYPE_FLOAT, 4, ", "));
+	printf("\n[%s]", strjoin(testdoubles, TYPE_DOUBLE, 4, ", "));
+
+	free(teststrs[0]);
+	free(teststrs[1]);
+	free(teststrs[2]);
+	free(teststrs[3]);
+	free(teststrs[4]);
+	free(teststrs);
 
 	return 0;
 }
